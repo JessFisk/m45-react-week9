c9fb78e8fab2e689ddc9ead0f69d5473a57a3aa7
Jessica Fisk <fisk.jess@gmail.com>
2023-03-09 20:43:45 +0000

    register and login working with front end react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
+import UserWrapper from "./components/userWrapper/UserWrapper";
+
 
 function App() {
+  const [user, setUser] = useState({
+    username: null,
+    email: null,
+    token: null,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "app-wrapper">
+      <h2>Hello World</h2>
+      <UserWrapper user = {user} setUser={setUser}/>
+    
+    <>{user.token ? (
+    <p>{user.username} is Logged in</p> ) : ( <p>Not Logged in</p>)}</>
     </div>
+
   );
 }
 
---
+++ src/components/userWrapper/UserWrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./UserWrapper.css";
+import LoginUser from "../loginUser/LoginUser";
+import AddUser from "../addUser/AddUser";
+
+const UserWrapper = ({user, setUser}) => {
+    return (
+    <div className= "user-wrapper">
+        <AddUser user= {user} setUser={setUser}/>
+        <LoginUser user= {user} setUser={setUser}/>
+    </div>
+    )
+}
+export default UserWrapper;
